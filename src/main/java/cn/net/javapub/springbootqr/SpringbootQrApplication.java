d55b5f32f21847b861848152e893a0fd18a7e398
rodert <731444260@qq.com>
2023-10-08 08:39:30 +0800

    init

---
+++ src/main/java/cn/net/javapub/springbootqr/SpringbootQrApplication.java
@@ -0,0 +1,13 @@
+package cn.net.javapub.springbootqr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootQrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootQrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/net/javapub/springbootqr/SpringbootQrApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.net.javapub.springbootqr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootQrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
